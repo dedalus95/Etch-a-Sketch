b0db960dfe73327065740828f7700871416222d5
Emanuele <ecerilli1@gmail.com>
2021-07-02 13:35:24 +0200

    update main

--- script.js
+++ script.js
@@ -2,80 +2,75 @@
 const btn = document.createElement('button');
 const container = document.getElementById('container');
 const btnReload = document.createElement('button');
+let hue = 0;
+container.before(btn);
+container.before(btnReload);
 
 
 
-
-
+document.body.style.backgroundColor = '#00703C';
 container.style.display = 'grid';
-// container.style.gridTemplateColumns = 'repeat(16, 1fr)';
-// container.style.gridTemplateRows = 'repeat(16, 1fr)';
-container.style.width = '605px';
-container.style.height = '605px';
-container.style.border = 'groove';
-container.style.gap = '-30px';
+container.style.width = '700px';
+container.style.height = '700px';
+container.style.border = 'solid';
+container.style.margin = '100px auto auto auto';
+container.style.background = 'white';
 
 btn.style.width = '200px';
 btn.style.height = '80px';
 btn.textContent = 'Set number of squares';
-btn.style.color = 'pink';
-btn.style.fontFamily = 'Times new roman';
+btn.style.color = '00703C';
+btn.style.fontFamily = 'Sans serif';
 btn.style.fontSize = '20px';
-btn.style.background = 'black';
-btn.style.margin = 'auto';
+btn.style.background = 'white';
+btn.style.margin = '50px';
 
 btnReload.style.width = '200px';
-btnReload.textContent = 'CLEAR GRID';
+btnReload.textContent = 'Clear grid';
 btnReload.style.height = '80px';
-btnReload.style.color = 'pink';
-btnReload.style.fontFamily = 'Times new roman';
+btnReload.style.color = '00703C';
+btnReload.style.fontFamily = 'Sans serif';
 btnReload.style.fontSize = '20px';
-btnReload.style.background = 'black';
-btnReload.style.margin = 'auto';
+btnReload.style.background = 'white';
+btnReload.style.margin = '50px';
 
-container.before(btn);
-container.before(btnReload)
+
 let numberOfSquares;
-
-// create a grid from scratch
-
-
-
-   
-    
-    
 
 
 
 function createDiv() {
 
     const div = document.createElement('div');
+
     
-    
-    div.style.background = 'pink';
-    div.style.width = '300px';
-    div.style.height = '300px';
-    div.style.border = 'solid';
-    div.style.borderColor = 'rgb(200,200,200)';
+    div.style.background = 'white';
+    // div.style.width = '300px';
+    // div.style.height = '300px';
+    div.style.border = 'groove';
+    div.style.borderColor = 'rgb(240,240,240)';
   
 
-    div.addEventListener('mousemove', function(colors) {
-         colors =  MouseEvent.clientX;
-        div.style.background = `rgb(24,143,243)`;
+  
+
+    div.addEventListener('mousemove', function(e) {
+       
+        div.style.background = `hsl(${hue}, 100%, 50%)`;
+        hue++;
+        if(hue > 360) {
+            hue = 0;
+        }
+        console.log(hue);
+
     });
     
     container.appendChild(div);
 
     
-    }
-   
+}
 
-    numberOfSquares = prompt('How many squares for side?');
 
-    container.style.gridTemplateColumns = `repeat(${numberOfSquares}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${numberOfSquares}, 1fr)`;
-
-        
+        function askForSquares() { 
 
         numberOfSquares = prompt('How many squares for side (max: 100)?');
 
@@ -83,24 +78,36 @@ function createDiv() {
             alert('invalid choice');
             return false;
         }
-    
-        container.style.gridTemplateColumns = `repeat(${numberOfSquares}, minmax(1px, 300px))`;
-        container.style.gridTemplateRows = `repeat(${numberOfSquares}, minmax(1px, 300px))`;
-
         
+    
+  
 
-        for (i = 0; i < numberOfSquares*numberOfSquares; i++) {
-            createDiv();
+    
+        container.style.gridTemplateColumns = `repeat(${numberOfSquares}, minmax(0, 1fr))`;
+        container.style.gridTemplateRows = `repeat(${numberOfSquares}, minmax(0, 1fr))`;
+       
+         for (i = 0; i < numberOfSquares*numberOfSquares; i++) {
+                    createDiv();
 
+                }
+        if (numberOfSquares) {
+            btn.disabled = true;
         }
 
+    }
+
+       
     
 
     function refreshPage() {
         window.location.reload()
     }
 
+
+
+    btn.addEventListener('click', askForSquares);
     btnReload.addEventListener('click', refreshPage);
+
 
 
 
